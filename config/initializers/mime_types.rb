cd641737c332b1210f8881fc50f62590fb83d7a4
root <root@webdevserver>
2014-03-21 18:51:35 +0800

    03212014

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,6 +4,6 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::type.register "image/png", :png
-Mime::type.register "image/jpg", :jpg
-Mime::type.register "image/jpeg", :jpeg
+Mime::Type.register "image/png", :png
+Mime::Type.register "image/jpg", :jpg
+Mime::Type.register "image/jpeg", :jpeg
